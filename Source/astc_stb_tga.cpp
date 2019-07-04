efaffdbb1885e1ae7c9ede6020de5900ce16fc5d
Pete Harris <pete@solidpixel.org>
2019-07-04 23:31:18 +0100

    Update to stb_image version 2.22 (#48)

--- Source/astc_stb_tga.cpp
+++ Source/astc_stb_tga.cpp
@@ -20,7 +20,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define STBI_HEADER_FILE_ONLY
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_JPEG
+#define STBI_NO_PSD
+#define STBI_NO_GIF
+#define STBI_NO_PIC
+#define STBI_NO_PNM
 #include "stb_image.c"
 
 astc_codec_image * load_image_with_stb(const char *filename, int padding, int *result)
