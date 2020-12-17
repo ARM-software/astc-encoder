ea93ff656712db5bec6658a62493055b29a8f1d0
Peter Harris <peter.harris@arm.com>
2020-12-17 20:11:46 +0000

    Add Coverity to Dockerfile

--- jenkins/build-image.sh
+++ jenkins/build-image.sh
@@ -24,7 +24,7 @@ pushd tmp
 popd
 
 echo "Building image"
-docker build -f build.Dockerfile \
+docker build -f jenkins/build.Dockerfile \
     -t $IMAGE_NAME:latest \
     -t $IMAGE_NAME:$IMAGE_VERSION \
     -t $DOCKER_REGISTRY/$IMAGE_NAME:latest \
