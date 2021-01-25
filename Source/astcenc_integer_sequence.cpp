e8e4e88d7635a7f5557b220d7dc5cd5d3adf904c
pete <peterharris747@gmail.com>
2021-01-25 23:47:44 +0000

    Use static table for ise_encoding

--- Source/astcenc_integer_sequence.cpp
+++ Source/astcenc_integer_sequence.cpp
@@ -21,6 +21,8 @@
 
 #include "astcenc_internal.h"
 
+#include <array>
+
 // unpacked quint triplets <low,middle,high> for each packed-quint value
 static const uint8_t quints_of_integer[128][3] = {
 	{0, 0, 0}, {1, 0, 0}, {2, 0, 0}, {3, 0, 0},
@@ -329,95 +331,36 @@ static const uint8_t integer_of_trits[3][3][3][3][3] = {
 	}
 };
 
-static void find_number_of_bits_trits_quints(
-	int quantization_level,
-	int* bits,
-	int* trits,
-	int* quints
-) {
-	*bits = 0;
-	*trits = 0;
-	*quints = 0;
-	switch (quantization_level)
-	{
-	case QUANT_2:
-		*bits = 1;
-		break;
-	case QUANT_3:
-		*bits = 0;
-		*trits = 1;
-		break;
-	case QUANT_4:
-		*bits = 2;
-		break;
-	case QUANT_5:
-		*bits = 0;
-		*quints = 1;
-		break;
-	case QUANT_6:
-		*bits = 1;
-		*trits = 1;
-		break;
-	case QUANT_8:
-		*bits = 3;
-		break;
-	case QUANT_10:
-		*bits = 1;
-		*quints = 1;
-		break;
-	case QUANT_12:
-		*bits = 2;
-		*trits = 1;
-		break;
-	case QUANT_16:
-		*bits = 4;
-		break;
-	case QUANT_20:
-		*bits = 2;
-		*quints = 1;
-		break;
-	case QUANT_24:
-		*bits = 3;
-		*trits = 1;
-		break;
-	case QUANT_32:
-		*bits = 5;
-		break;
-	case QUANT_40:
-		*bits = 3;
-		*quints = 1;
-		break;
-	case QUANT_48:
-		*bits = 4;
-		*trits = 1;
-		break;
-	case QUANT_64:
-		*bits = 6;
-		break;
-	case QUANT_80:
-		*bits = 4;
-		*quints = 1;
-		break;
-	case QUANT_96:
-		*bits = 5;
-		*trits = 1;
-		break;
-	case QUANT_128:
-		*bits = 7;
-		break;
-	case QUANT_160:
-		*bits = 5;
-		*quints = 1;
-		break;
-	case QUANT_192:
-		*bits = 6;
-		*trits = 1;
-		break;
-	case QUANT_256:
-		*bits = 8;
-		break;
-	}
-}
+struct btq_count {
+	uint8_t quant;
+	uint8_t bits;
+	uint8_t trits;
+	uint8_t quints;
+};
+
+static std::array<btq_count, 21> btq_counts = {{
+	{   QUANT_2, 1, 0, 0 },
+	{   QUANT_3, 0, 1, 0 },
+	{   QUANT_4, 2, 0, 0 },
+	{   QUANT_5, 0, 0, 1 },
+	{   QUANT_6, 1, 1, 0 },
+	{   QUANT_8, 3, 0, 0 },
+	{  QUANT_10, 1, 0, 1 },
+	{  QUANT_12, 2, 1, 0 },
+	{  QUANT_16, 4, 0, 0 },
+	{  QUANT_20, 2, 0, 1 },
+	{  QUANT_24, 3, 1, 0 },
+	{  QUANT_32, 5, 0, 0 },
+	{  QUANT_40, 3, 0, 1 },
+	{  QUANT_48, 4, 1, 0 },
+	{  QUANT_64, 6, 0, 0 },
+	{  QUANT_80, 4, 0, 1 },
+	{  QUANT_96, 5, 1, 0 },
+	{ QUANT_128, 7, 0, 0 },
+	{ QUANT_160, 5, 0, 1 },
+	{ QUANT_192, 6, 1, 0 },
+	{ QUANT_256, 8, 0, 0 }
+}};
 
 // routine to write up to 8 bits
 static inline void write_bits(
@@ -466,8 +409,9 @@ void encode_ise(
 	uint8_t highparts[69];		// 64 elements + 5 elements for padding
 	uint8_t tq_blocks[22];		// trit-blocks or quint-blocks
 
-	int bits, trits, quints;
-	find_number_of_bits_trits_quints(quantization_level, &bits, &trits, &quints);
+	int bits = btq_counts[quantization_level].bits;
+	int trits = btq_counts[quantization_level].trits;
+	int quints = btq_counts[quantization_level].quints;
 
 	promise(elements > 0);
 	for (int i = 0; i < elements; i++)
@@ -548,8 +492,9 @@ void decode_ise(
 	uint8_t results[68];
 	uint8_t tq_blocks[22];		// trit-blocks or quint-blocks
 
-	int bits, trits, quints;
-	find_number_of_bits_trits_quints(quantization_level, &bits, &trits, &quints);
+	int bits = btq_counts[quantization_level].bits;
+	int trits = btq_counts[quantization_level].trits;
+	int quints = btq_counts[quantization_level].quints;
 
 	int lcounter = 0;
 	int hcounter = 0;
