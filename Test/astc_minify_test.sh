cf3de1d8ad2cb41c189bc4f16359fcaebdf8f01e
pete <peterharris747@gmail.com>
2021-02-19 01:03:15 +0000

    Improve minify test script

--- Test/astc_minify_test.sh
+++ Test/astc_minify_test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-./astcenc/astcenc-avx2-good -tl in.png out-good.png 6x6 -medium
+BLOCK_SIZE=6x6
+QUALITY=-thorough
 
-./astcenc/astcenc-avx2 -tl in.png out-bad.png 6x6 -medium
+./astcenc/astcenc-avx2-good -tl in.png out-good.png ${BLOCK_SIZE} ${QUALITY} -silent
 
-./astcenc/astc_test_autoextract 6x6 in.png out-good.png out-bad.png in-min.png
+./astcenc/astcenc-avx2 -tl in.png out-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
+
+compare out-good.png out-bad.png out-vs-out-diff.png
+compare in.png out-bad.png out-vs-in-diff.png
+
+./astcenc/astc_test_autoextract ${BLOCK_SIZE} in.png out-good.png out-bad.png in-min.png
+
+./astcenc/astcenc-avx2-good -tl in-min.png out-min-good.png ${BLOCK_SIZE} ${QUALITY} -silent
+
+./astcenc/astcenc-avx2 -tl in-min.png out-min-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
+
+compare out-min-good.png out-min-bad.png out-min-diff.png
