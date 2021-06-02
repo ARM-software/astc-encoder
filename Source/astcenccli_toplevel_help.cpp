e9ffe95e1926d6364a3578f8ccc34ce8ea2b905a
pete <peterharris747@gmail.com>
2021-06-02 23:28:37 +0100

    Update version for 3.0 release

--- Source/astcenccli_toplevel_help.cpp
+++ Source/astcenccli_toplevel_help.cpp
@@ -23,7 +23,7 @@
 
 /** @brief The version header. */
 static const char *astcenc_copyright_string =
-R"(astcenc v3.0-develop, %u-bit %s%s
+R"(astcenc v3.0, %u-bit %s%s
 Copyright 2011-2021 Arm Limited, all rights reserved
 )";
 
