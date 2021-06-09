2fa1493d18cf3b3b4ecc978c7a4becf6352b3ebb
pete <peterharris747@gmail.com>
2021-06-09 16:11:47 +0100

    Add issue reference to MSVC workaround comment

--- Source/astcenc_vecmathlib.h
+++ Source/astcenc_vecmathlib.h
@@ -431,7 +431,8 @@ static ASTCENC_SIMD_INLINE vint4 unorm16_to_sf16(vint4 p)
 	vmask4 is_one = p == vint4(0xFFFF);
 	vmask4 is_small = p < vint4(4);
 
-// Manually inline clz() on Visual Studio to avoid release build codegen bug
+	// Manually inline clz() on Visual Studio to avoid release build codegen bug
+	// see https://github.com/ARM-software/astc-encoder/issues/259
 #if !defined(__clang__) && defined(_MSC_VER)
 	vint4 a = (~lsr<8>(p)) & p;
 	a = float_as_int(int_to_float(a));
