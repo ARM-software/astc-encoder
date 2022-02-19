aa06d0da1e48fb17eef56bd339c9cdb7f5977472
Pete Harris <peter.harris@arm.com>
2022-02-19 21:56:47 +0000

    Update copyright years

--- Source/astcenc.h
+++ Source/astcenc.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // ----------------------------------------------------------------------------
-// Copyright 2020-2021 Arm Limited
+// Copyright 2020-2022 Arm Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
