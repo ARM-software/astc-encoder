1176aded91cbcff20e40dd77c810a793f4548813
Pete Harris <peter.harris@arm.com>
2022-06-06 08:37:32 +0100

    Make it easier to minify tests on Windows

--- Test/astc_minify_test.sh
+++ Test/astc_minify_test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-BLOCK_SIZE=6x6
-QUALITY=-thorough
+GOOD=astcenc-good.exe
+BAD=astcenc-avx2.exe
+BLOCK_SIZE=4x4
+QUALITY=-fastest
 
-./astcenc/astcenc-avx2-good -tl in.png out-good.png ${BLOCK_SIZE} ${QUALITY} -silent
+./astcenc/${GOOD} -tl in.png out-good.png ${BLOCK_SIZE} ${QUALITY} -silent
 
-./astcenc/astcenc-avx2 -tl in.png out-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
+./astcenc/${BAD} -tl in.png out-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
 
 compare out-good.png out-bad.png out-vs-out-diff.png
 compare in.png out-bad.png out-vs-in-diff.png
 
 ./astcenc/astc_test_autoextract ${BLOCK_SIZE} in.png out-good.png out-bad.png in-min.png
 
-./astcenc/astcenc-avx2-good -tl in-min.png out-min-good.png ${BLOCK_SIZE} ${QUALITY} -silent
+./astcenc/${GOOD} -tl in-min.png out-min-good.png ${BLOCK_SIZE} ${QUALITY} -silent
 
-./astcenc/astcenc-avx2 -tl in-min.png out-min-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
+./astcenc/${BAD} -tl in-min.png out-min-bad.png ${BLOCK_SIZE} ${QUALITY} -silent
 
 compare out-min-good.png out-min-bad.png out-min-diff.png
