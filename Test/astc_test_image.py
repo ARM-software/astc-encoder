c99ba37f2fa454199f59e707a65ac8a72e4fa4ed
Peter Harris <peter.harris@arm.com>
2022-10-29 18:49:53 +0100

    Add -verythorough to tests

--- Test/astc_test_image.py
+++ Test/astc_test_image.py
@@ -49,7 +49,7 @@ RESULT_THRESHOLD_3D_FAIL = -0.00
 
 TEST_BLOCK_SIZES = ["4x4", "5x5", "6x6", "8x8", "12x12", "3x3x3", "6x6x6"]
 
-TEST_QUALITIES = ["fastest", "fast", "medium", "thorough", "exhaustive"]
+TEST_QUALITIES = ["fastest", "fast", "medium", "thorough", "verythorough", "exhaustive"]
 
 
 def is_3d(blockSize):
