64c827e5dd5de87fd78d520c73bf75d8033fac4b
Peter Harris <peter.harris@arm.com>
2026-03-26 10:04:02 +0000

    Add unit test

--- Source/UnitTest/test_decode.cpp
+++ Source/UnitTest/test_decode.cpp
@@ -75,6 +75,58 @@ TEST(decode, decode12x12)
 		}
 	}
 #endif
+
+	astcenc_context_free(context);
+}
+
+/** @brief Test harness for context inheritance. */
+TEST(decode, context_inherit)
+{
+	astcenc_error status;
+	astcenc_config config;
+	astcenc_context* parent_context;
+	astcenc_context* context;
+	astcenc_context* error_context;
+
+	static const astcenc_swizzle swizzle {
+		ASTCENC_SWZ_R, ASTCENC_SWZ_G, ASTCENC_SWZ_B, ASTCENC_SWZ_A
+	};
+
+	uint8_t data[16] {
+#if 0
+		0x84,0x00,0x38,0xC8,0x00,0x00,0x00,0x00,
+		0x00,0x00,0x00,0x00,0x00,0xB3,0x4D,0x78
+#else
+		0x29,0x00,0x1A,0x97,0x01,0x00,0x00,0x00,
+		0x00,0x00,0x00,0x00,0x00,0xCF,0x97,0x86
+#endif
+	};
+
+	uint8_t output[12*12*4];
+	astcenc_config_init(ASTCENC_PRF_LDR, 12, 12, 1, ASTCENC_PRE_MEDIUM, 0, &config);
+
+	status = astcenc_context_alloc(&config, 1, &parent_context, nullptr);
+	EXPECT_EQ(status, ASTCENC_SUCCESS);
+
+	status = astcenc_context_alloc(nullptr, 1, &context, parent_context);
+	EXPECT_EQ(status, ASTCENC_SUCCESS);
+
+	status = astcenc_context_alloc(&config, 1, &error_context, parent_context);
+	EXPECT_EQ(status, ASTCENC_ERR_BAD_PARAM);
+
+	astcenc_image image;
+	image.dim_x = 12;
+	image.dim_y = 12;
+	image.dim_z = 1;
+	image.data_type = ASTCENC_TYPE_U8;
+	uint8_t* slices = output;
+	image.data = reinterpret_cast<void**>(&slices);
+
+	status = astcenc_decompress_image(context, data, 16, &image, &swizzle, 0);
+	EXPECT_EQ(status, ASTCENC_SUCCESS);
+
+	astcenc_context_free(context);
+	astcenc_context_free(parent_context);
 }
 
 }
