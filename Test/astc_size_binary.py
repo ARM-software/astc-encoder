749e891ffb3ca154d55946c1a96d52de52a8dbde
Peter Harris <peter.harris@arm.com>
2026-05-01 13:59:10 +0100

    Cleanup astc_size_binary.py

--- Test/astc_size_binary.py
+++ Test/astc_size_binary.py
@@ -15,25 +15,22 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 # -----------------------------------------------------------------------------
-"""
-The ``astc_size_binary`` utility provides a wrapper around the Linux ``size``
-utility to view binary section sizes, and optionally compare the section sizes
-of two binaries. Section sizes are given for code (``.text``), read-only data
-(``.rodata``), and zero initialized data (``.bss``). All other sections are
-ignored.
+'''
+This script is a wrapper around the Linux and macOS 'size' utility to extract
+information about binary section sizes, and optionally compare the section
+sizes of two binaries.
 
-A typical report comparing the size of a new binary against a reference looks
-like this:
+Section sizes are reported for code (.text), read-only data (.rodata), and zero
+initialized data (.bss) sections. All other sections are ignored.
 
-.. code-block::
+A typical report comparing the size of two binaries looks like this:
 
-              Code   RO Data   ZI Data
-     Ref    411298    374560    128576
-     New    560530     89552     31744
-   Abs D    149232   -285008    -96832
-   Rel D    36.28%   -76.09%   -75.31%
-"""
-
+               Code   RO Data   ZI Data
+      Ref    411298    374560    128576
+      New    560530     89552     31744
+    Abs D    149232   -285008    -96832
+    Rel D    36.28%   -76.09%   -75.31%
+'''
 
 import argparse
 import platform
@@ -42,166 +39,162 @@ import subprocess as sp
 import sys
 
 
-def run_size_linux(binary):
-    """
+def run_size_linux(binary: str) -> tuple[int, int, int]:
+    '''
     Run size on a single binary.
 
     Args:
-        binary (str): The path of the binary file to process.
+        binary: The path of the binary file to analyze.
 
-    Returns:
-        tuple(int, int, int): A triplet of code size, read-only data size, and
-        zero-init data size, all in bytes.
+    Return:
+        A triplet of code size, RO data size, ZI data size.
 
-    Raises:
-        CalledProcessException: The ``size`` subprocess failed for any reason.
-    """
-    args = ["size", "--format=sysv", binary]
+    Raise:
+        CalledProcessException: The size subprocess failed.
+    '''
+    args = ['size', '--format=sysv', binary]
     result = sp.run(args, stdout=sp.PIPE, stderr=sp.PIPE,
-                    check=True, universal_newlines=True)
+                    check=True, text=True)
 
     data = {}
-    patterns = {"Code": ".text", "RO": ".rodata", "ZI": ".bss"}
+    patterns = {'Code': '.text', 'RO': '.rodata', 'ZI': '.bss'}
 
-    lines = result.stdout.splitlines()
-    for line in lines:
+    for line in result.stdout.splitlines():
         for key, value in patterns.items():
             if line.startswith(value):
-                size = float(line.split()[1])
+                size = int(line.split()[1])
                 data[key] = size
 
-    return (data["Code"], data["RO"], data["ZI"])
+    return (data['Code'], data['RO'], data['ZI'])
 
 
-def run_size_macos(binary):
-    """
+def run_size_macos(binary: str) -> tuple[int, int, int]:
+    '''
     Run size on a single binary.
 
     Args:
-        binary (str): The path of the binary file to process.
+        binary: The path of the binary file to analyze.
 
-    Returns:
-        tuple(int, int, int): A triplet of code size, read-only data size, and
-        zero-init data size, all in bytes.
+    Return:
+        A triplet of code size, RO data size, ZI data size.
 
-    Raises:
-        CalledProcessException: The ``size`` subprocess failed for any reason.
-    """
-    args = ["size", "-m", binary]
+    Raise:
+        CalledProcessException: The size subprocess failed.
+    '''
+    args = ['size', '-m', binary]
     result = sp.run(args, stdout=sp.PIPE, stderr=sp.PIPE,
-                    check=True, universal_newlines=True)
+                    check=True, text=True)
 
-    code = 0
-    dataRO = 0
-    dataZI = 0
+    size_code = 0
+    size_ro = 0
+    size_zi = 0
 
-    currentSegment = None
+    current_segment = None
 
-    lines = result.stdout.splitlines()
-    for line in lines:
+    for line in result.stdout.splitlines():
         line = line.strip()
 
-        if line.startswith("Segment"):
+        if line.startswith('Segment'):
             parts = line.split()
             assert len(parts) >= 3, parts
 
-            currentSegment = parts[1]
+            current_segment = parts[1]
             size = int(parts[2])
 
-            if currentSegment == "__TEXT:":
-                code += size
+            if current_segment == '__TEXT:':
+                size_code += size
 
-            if currentSegment == "__DATA_CONST:":
-                dataRO += size
+            if current_segment == '__DATA_CONST:':
+                size_ro += size
 
-            if currentSegment == "__DATA:":
-                dataZI += size
+            if current_segment == '__DATA:':
+                size_zi += size
 
-        if line.startswith("Section"):
+        if line.startswith('Section'):
             parts = line.split()
             assert len(parts) >= 3, parts
 
             section = parts[1]
             size = int(parts[2])
 
-            if currentSegment == "__TEXT:" and section == "__const:":
-                code -= size
-                dataRO += size
+            if current_segment == '__TEXT:' and section == '__const:':
+                size_code -= size
+                size_ro += size
 
-    return (code, dataRO, dataZI)
+    return (size_code, size_ro, size_zi)
 
 
-def parse_command_line():
-    """
+def parse_command_line() -> argparse.Namespace:
+    '''
     Parse the command line.
 
-    Returns:
-        Namespace: The parsed command line container.
-    """
+    Return:
+        The parsed command line.
+    '''
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("bin", type=argparse.FileType("r"),
-                        help="The new binary to size")
+    parser.add_argument('bin', type=argparse.FileType('r'),
+                        help='the binary to size')
 
-    parser.add_argument("ref", nargs="?", type=argparse.FileType("r"),
-                        help="The reference binary to compare against")
+    parser.add_argument('ref_bin', nargs='?', type=argparse.FileType('r'),
+                        help='the reference binary to compare against')
 
     return parser.parse_args()
 
 
-def main():
-    """
+def main() -> int:
+    '''
     The main function.
 
-    Returns:
-        int: The process return code.
-    """
+    Return:
+        The process return code.
+    '''
     args = parse_command_line()
 
-    # Preflight - check that size exists. Note that size might still fail at
-    # runtime later, e.g. if the binary is not of the correct format
-    path = shutil.which("size")
+    # Preflight - check that size exists
+    path = shutil.which('size')
     if not path:
-        print("ERROR: The 'size' utility is not installed on the PATH")
+        print('ERROR: The size utility is not installed')
         return 1
 
-    if platform.system() == "Darwin":
+    # Pick an appropriate implementation based on host OS
+    run_size = run_size_linux
+    if platform.system() == 'Darwin':
         run_size = run_size_macos
-    else:
-        run_size = run_size_linux
 
     # Collect the data
     try:
-        newSize = run_size(args.bin.name)
-        if args.ref:
-            refSize = run_size(args.ref.name)
+        ref_dat = None
+        if args.ref_bin:
+            ref_dat = run_size(args.ref_bin.name)
+
+        new_dat = run_size(args.bin.name)
+
     except sp.CalledProcessError as ex:
-        print("ERROR: The 'size' utility failed")
-        print("       %s" % ex.stderr.strip())
+        print('ERROR: The size utility failed')
+        print(ex.stderr.strip())
         return 1
 
-    # Print the basic table of absolute values
-    print("%8s  % 8s  % 8s  % 8s" % ("", "Code", "RO Data", "ZI Data"))
-    if args.ref:
-        print("%8s  % 8u  % 8u  % 8u" % ("Ref", *refSize))
-    print("%8s  % 8u  % 8u  % 8u" % ("New", *newSize))
+    # Common header
+    print('              Code        RO        ZI')
 
-    # Print the difference if we have a reference
-    if args.ref:
-        diffAbs = []
-        diffRel = []
-        for refVal, newVal in zip(refSize, newSize):
-            diff = newVal - refVal
-            diffAbs.append(diff)
-            diffRel.append((diff / refVal) * 100.0)
+    # One binary, no reference comparison
+    if not ref_dat:
+        print(f'     New{new_dat[0]:>10}{new_dat[1]:>10}{new_dat[2]:>10}')
 
-        dat = ("Abs D", diffAbs[0], diffAbs[1], diffAbs[2])
-        print("%8s  % 8u  % 8u  % 8u" % dat)
-        dat = ("Rel D", diffRel[0], diffRel[1], diffRel[2])
-        print("%8s  % 7.2f%%  % 7.2f%%  % 7.2f%%" % dat)
+    # Two binaries with reference comparison
+    else:
+        print(f'     Ref{ref_dat[0]:>10}{ref_dat[1]:>10}{ref_dat[2]:>10}')
+        print(f'     New{new_dat[0]:>10}{new_dat[1]:>10}{new_dat[2]:>10}')
+
+        df_a = [y - x for (x, y) in zip(ref_dat, new_dat)]
+        df_r = [((y - x) / x) * 100.0 for (x, y) in zip(ref_dat, new_dat)]
+
+        print(f'Diff abs{df_a[0]:>+10}{df_a[1]:>+10}{df_a[2]:>+10}')
+        print(f'Diff rel{df_r[0]:>+10.2f}%{df_r[1]:>+9.2f}%{df_r[2]:>+9.2f}%')
 
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
