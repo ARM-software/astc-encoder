c9ae4fe99f8b9531fec69ccc94f9e66e1abb1289
Peter Harris <peter.harris@arm.com>
2026-06-03 00:32:34 +0100

    Use RAII for float storage scratch buffers

--- Source/astcenccli_internal.h
+++ Source/astcenccli_internal.h
@@ -308,32 +308,16 @@ astcenc_image_ptr astc_img_from_unorm8x4_array(
 /**
  * @brief Create a flattened RGBA FLOAT32 data array for a single slice from an image structure.
  *
- * The returned data array is allocated with @c new[] and must be freed with a @c delete[] call.
- *
  * @param img       The input image.
  * @param y_flip    Should the data in the array be Y flipped?
  * @param z_index   The slice index to convert.
  *
  * @return The data array.
  */
-float* floatx4_array_from_astc_img(
+std::vector<float> floatx4_array_from_astc_img(
 	const astcenc_image* img,
 	bool y_flip,
 	unsigned int z_index);
-
-/**
- * @brief Create a flattened RGBA UNORM8 data array from an image structure.
- *
- * The returned data array is allocated with @c new[] and must be freed with a @c delete[] call.
- *
- * @param img      The input image.
- * @param y_flip   Should the data in the array be Y flipped?
- *
- * @return The data array.
- */
-uint8_t* unorm8x4_array_from_astc_img(
-	const astcenc_image* img,
-	bool y_flip);
 
 /* ============================================================================
   Functions for printing build info and help messages
