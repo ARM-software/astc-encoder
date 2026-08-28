8567a4812864d113b9d179aa065278b4ecf2af6e
Peter Harris <peter.harris@arm.com>
2026-08-28 20:14:41 +0100

    Improve test reference rebuilds
    Make reference rebuilds an explicit test run target, rather than inferred.
    Make test quality an option list rather than a single option.

--- Test/astc_update_ref.sh
+++ Test/astc_update_ref.sh
@@ -8,7 +8,7 @@ fi
 echo "Generating new ref-$1 results"
 
 if [ "$1" = "main" ]; then
-    echo "Using binary from ./bin/${1}/"
+    echo "Using binary from ./bin/"
 else
     echo "Using binary from ./Binaries/${1}/"
 fi
@@ -17,7 +17,7 @@ echo ""
 
 TARGET_ROOT=${1}
 
-python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --encoder ref-$1-avx2
-#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --encoder ref-$1-sse4.1
-#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --encoder ref-$1-sse2
-#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --encoder ref-$1-neon -j3
+python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --reference rebuild --encoder ref-$1-avx2
+#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --reference rebuild --encoder ref-$1-sse4.1
+#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --reference rebuild --encoder ref-$1-sse2
+#python3 ./Test/astc_test_image.py --test-set all --block-size all --test-quality all --repeats 6 --reference rebuild --encoder ref-$1-neon -j3
